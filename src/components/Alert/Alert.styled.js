8ad5d7806e98154d196891f7b94af6e851af9a26
AnnaBuller <aniabela@wp.pl>
2021-01-18 16:04:35 +0100

    add default and variant style

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
-    display: block;
+const DefaultAlert = styled.div`
+    background-color: #cce5ff;
+    border-color: #b8daff;
+    border-radius: .25rem;
+    border: 1px solid transparent;
+    color: #004085;
+    margin-bottom: 1rem;
+    padding: .75rem 1.25rem;
+    position: relative;
+`
+
+const StyledAlert = styled(DefaultAlert)`
+    background-color: ${({theme, variant}) => variant && theme[variant].backgroundColor};
 `
 
 export { StyledAlert };
