557c1a1ce8a4dde5d9b45f2277809a98ca83f99b
AnnaBuller <aniabela@wp.pl>
2021-01-18 16:03:50 +0100

    add ThemeProvider and theme

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
+import theme from '../src/components/Alert/theme'
 
 import Alert from './../src/components/Alert';
 import { Row, Col, Alert as RBAlert } from 'react-bootstrap';
@@ -10,7 +12,9 @@ const Task01 = () => {
                 <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
             </Col>
             <Col>
-                <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                <ThemeProvider theme={theme}>
+                    <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                </ThemeProvider>
             </Col>
         </Row>
     )
