a3271998cffb9c75ac8a85908b15747dfa4f96ef
AnnaBuller <aniabela@wp.pl>
2021-01-20 15:56:46 +0100

    add style

---
+++ src/components/Breadcrumb/BreadcrumbItem.styled.js
@@ -0,0 +1,24 @@
+import styled, {css} from 'styled-components';
+
+const activeStyles = css`
+    color: #6c757d
+`
+const BreadcrumbItem = styled.li`
+    &+li {
+        padding-left: .5rem;
+    };
+    &+li:before {
+        display: inline-block;
+        padding-right: .5rem;
+        color: #6c757d;
+        content: "/";
+    }
+    &>a {
+        color: #007bff;
+        text-decoration: none;
+        background-color: transparent;
+    };
+    ${props => props.active && activeStyles}
+`
+
+export default BreadcrumbItem
