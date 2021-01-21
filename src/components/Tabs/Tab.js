3a88e43eb64954aaf4df07fd79b9da49a7dd91e7
AnnaBuller <aniabela@wp.pl>
2021-01-21 10:59:26 +0100

    add onClick event and console.log

--- src/components/Tabs/Tab.js
+++ src/components/Tabs/Tab.js
@@ -2,9 +2,10 @@ import React from 'react';
 import StyledTab from './Tab.styled'
 
 const Tab = props => {
-    const {eventKey, title, disabled, active} = props;
+    const {eventKey, title, disabled, active, onClick} = props;
+    console.log(eventKey + ' => przycisk jest aktywny: ' + active)
     return (
-        <StyledTab data-event-key={eventKey} href='#' disabled={disabled} active={active}>{title}</StyledTab>
+        <StyledTab onClick={e => onClick(e.target.dataset.eventKey)} data-event-key={eventKey} href='#' disabled={disabled} active={active}>{title}</StyledTab>
     )
 }
 
