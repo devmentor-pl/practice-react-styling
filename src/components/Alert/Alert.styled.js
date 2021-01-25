b3f2c8bdac8a993aa2010e30420ede0a7c90b256
Martyna Kiljan <kmartynak@icloud.com>
2021-01-25 16:07:45 +0100

    task _01

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,22 @@
+
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
-    display: block;
+	const DefaultAlert = styled.div`
+    display: block;	 
+    border-color: #b8daff;
+    border-radius: .25rem;
+    border: 1px solid transparent;
+    color: #004085;
+    margin-bottom: 1rem;
+    padding: .75rem 1.25rem;
+    position: relative;
 `
 
-export { StyledAlert };
+// `	
+const StyledAlert = styled(DefaultAlert)`
+    background-color: ${props =>  props.theme && props.variant ? props.theme[props.variant] : "#D3D3D3"};
+`	
+
+
+export { StyledAlert };  
+
--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,22 @@ import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
+import { ThemeProvider } from 'styled-components'
+
+
+    const themeSettings = {
+        primary:   '#cce5ff',
+        secondary: '#BBBFFF',
+        success:   '#5cb85c',
+        warning:   '#f0ad4e'
+    }
 
 
 const App = () => {
     return (
         <>
-            <Container fluid>
+           <ThemeProvider theme = {themeSettings}>
+           <Container fluid>
                 <Row>
                     <Col>
                         <h2>Komponenty React Boostrap</h2></Col>
@@ -25,6 +35,7 @@ const App = () => {
                 <Task04/>
                 <Task05/>
             </Container>
+           </ThemeProvider>
         </>
     )
 }
