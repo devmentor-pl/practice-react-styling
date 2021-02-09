c93ec10f06f980e3eed1ed0986bd532483935a69
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-02-09 10:31:05 +0100

    Task 02 ready

--- 02/Task02.js
+++ 02/Task02.js
@@ -3,18 +3,26 @@ import React from 'react';
 import Button from './../src/components/Button';
 import { Row, Col, Button as RBButton } from 'react-bootstrap';
 
+import { ThemeProvider } from 'styled-components';
+import themeSettings from './theme';
+
 const Task02 = () => {
-    return (
-        <Row>
-            <Col>
-                <RBButton variant="primary" size="lg">Button!</RBButton>
-            </Col>
-            <Col>
-                Button!
-            </Col>
-        </Row>
-)
-}
+  return (
+    <Row>
+      <Col>
+        <RBButton variant='primary' size='lg' disabled>
+          Button!
+        </RBButton>
+      </Col>
+      <Col>
+        <ThemeProvider theme={themeSettings}>
+          <Button variant='primary' size='lg' disabled>
+            Button!
+          </Button>
+        </ThemeProvider>
+      </Col>
+    </Row>
+  );
+};
 
 export default Task02;
-
