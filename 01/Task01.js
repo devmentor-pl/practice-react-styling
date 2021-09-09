7d213ad210715ec94c7a8234ed75691ff76bcc11
Dominik Mozdzen <dominik.mozdzen1@gmail.com>
2021-09-09 18:15:02 +0200

    Task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {ThemeProvider} from 'styled-components';
+import themeSettings from './../src/components/Alert/styled/theme';
 import Alert from './../src/components/Alert';
 import { Row, Col, Alert as RBAlert } from 'react-bootstrap';
 
@@ -10,7 +11,9 @@ const Task01 = () => {
                 <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
             </Col>
             <Col>
-                <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                <ThemeProvider theme={themeSettings}>
+                    <Alert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                </ThemeProvider>
             </Col>
         </Row>
     )
---
+++ src/components/Alert/styled/theme.js
@@ -0,0 +1,42 @@
+export default {
+	primary: {
+		color: '#004085',
+		backgroundColor: '#CCE5FF',
+        borderColor: '#B8DAFF'
+	},
+	secondary: {
+		color: '#383D41',
+		backgroundColor: '#E2E3E5',
+        borderColor: '#D6D8DB'
+	},
+	success: {
+		color: '#548860',
+		backgroundColor: '#D4EDDA',
+        borderColor: '#C3E6CB'
+	},
+	danger: {
+		color: '#7F2E36',
+		backgroundColor: '#F8D7DA',
+        borderColor: '#F5C2C7'
+	},
+	warning: {
+		color: '#907117',
+		backgroundColor: '#FFF3CD',
+        borderColor: '#FFECB5'
+	},
+	info: {
+		color: '#0C5460',
+		backgroundColor: '#D1ECF1',
+        borderColor: '#BEE5EB'
+	},
+	light: {
+		color: '#818182',
+		backgroundColor: '#FEFEFE',
+        borderColor: '#FEFEFE'
+	},
+	dark: {
+		color: '#1B1E21',
+		backgroundColor: '#D3D3D4',
+        borderColor: '#C6C8CA'
+	}
+}; 
