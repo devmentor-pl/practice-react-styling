bf17a805d09f9eeeac83da08f221c6fe88f632f8
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2022-01-17 22:46:35 +0100

    closes task 1

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const StyledAlert = styled.div`
-    display: block;
-`
+const DefaultStyledAlert = styled.div`
+  position: relative;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+`;
+
+const StyledAlert = styled(DefaultStyledAlert)(({ theme, variant }) => {
+  if (theme[variant]) {
+    const option = theme[variant];
+    return {
+      color: option.color,
+      backgroundColor: option.bgColor,
+      borderColor: option.borderColor,
+    };
+  }
+  return null;
+});
 
 export { StyledAlert };
