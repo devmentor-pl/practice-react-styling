15bfa8387241805791a056548adbb7a78cc78953
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2022-01-21 02:27:05 +0100

    closes task 5

---
+++ src/components/Card/CardTitle.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledCardTitle = styled.div`
+  margin-bottom: 0.75rem;
+  font-size: 1.25rem;
+  font-weight: 500;
+  line-height: 1.2;
+`;
+
+export default StyledCardTitle;
