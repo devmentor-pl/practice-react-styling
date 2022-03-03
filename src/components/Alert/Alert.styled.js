47e946a203e20cac573cf9a96afc87ee621d78ac
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2022-03-03 11:35:36 +0100

    Task 01 finished

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
+const defaultAlert = styled.div`
     display: block;
-`
+    margin-bottom: 1rem;
+    padding: 0.75rem 1.25rem;
+`;
+
+const StyledAlert = styled(defaultAlert)`
+    background-color: ${({ theme, variant = 'primary' }) => theme.color[variant].bg};
+    border-radius: ${({ theme }) => theme['border-radius']};
+    border: 1px solid ${({ theme, variant = 'primary' }) => theme.color[variant].border};
+    color: ${({ theme, variant = 'primary' }) => theme.color[variant].color};
+`;
 
 export { StyledAlert };
---
+++ src/styled/theme.js
@@ -0,0 +1,45 @@
+export default {
+    'border-radius': '0.25rem',
+    color: {
+        primary: {
+            bg: '#cfe2ff',
+            border: '#b6d4fe',
+            color: '#084298',
+        },
+        secondary: {
+            bg: '#e2e3e5',
+            border: '#d3d6d8',
+            color: '#41464b',
+        },
+        success: {
+            bg: '#d1e7dd',
+            border: '#badbcc',
+            color: '#0f5132',
+        },
+        danger: {
+            bg: '#f8d7da',
+            border: '#f5c2c7',
+            color: '#842029',
+        },
+        warning: {
+            bg: '#fff3cd',
+            border: '#ffecb5',
+            color: '#664d03',
+        },
+        info: {
+            bg: '#cff4fc',
+            border: '#b6effb',
+            color: '#055160',
+        },
+        light: {
+            bg: '#fefefe',
+            border: '#fdfdfe',
+            color: '#636464',
+        },
+        dark: {
+            bg: '#d3d3d4',
+            border: '#bcbebf',
+            color: '#141619',
+        },
+    },
+};
