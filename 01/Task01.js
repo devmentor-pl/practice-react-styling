3d2a80417bd943fe36b00e05f66222dd84884f1c
Rafał Kazik <kazik.raf@gmail.com>
2022-03-11 16:02:11 +0100

    1st task

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Alert from './../src/components/Alert';
 import { Row, Col, Alert as RBAlert } from 'react-bootstrap';
 
 const Task01 = () => {
-    return (
-        <Row>
-            <Col>
-                <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
-            </Col>
-            <Col>
-                <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
-            </Col>
-        </Row>
-    )
-}
+  return (
+    <Row>
+      <Col>
+        <RBAlert variant='primary'>
+          Uwaga! <em>Styled Components</em> nadchodzi!
+        </RBAlert>
+      </Col>
+      <Col>
+        <Alert>
+          Uwaga! <em>Styled Components</em> nadchodzi!
+        </Alert>
+      </Col>
+    </Row>
+  );
+};
 
 export default Task01;
-
