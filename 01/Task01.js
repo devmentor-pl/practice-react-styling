7d837c8f2e2f625d43e8fcb954eff5052bff5d03
Ewelina <ewelina@ekopacz.pl>
2022-04-15 21:23:28 +0200

    chnged naming

--- 01/Task01.js
+++ 01/Task01.js
@@ -10,8 +10,8 @@ const Task01 = () => {
                 <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
             </Col>
             <Col>
-                <Alert variant='info'>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
-                <Alert variant='error'>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                <Alert variant='primary'>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                <Alert variant='danger'>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
                 <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
             </Col>
         </Row>
