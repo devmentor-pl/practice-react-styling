d0086fd30e58b8f6888f82121f013a159cb219b0
Ewelina <ewelina@ekopacz.pl>
2022-04-15 21:22:42 +0200

    upgrade theme file

---
+++ src/styled/theme.js
@@ -0,0 +1,69 @@
+
+export default {
+    alerts:{
+        primary: {
+            color: '#004085',
+            borderColor: '#b8daff',
+            backgroundColor: '#cce5ff',
+        },
+
+        danger:{
+            color: '#721c24',
+            backgroundColor: '#f8d7da',
+            borderColor: '#f5c6cb',
+        },
+    },
+
+    buttons:{
+        primary:{
+            color: '#fff',
+            backgroundColor: '#007bff',
+            borderColor: '#007bff',
+        },
+        success:{
+            color: '#fff',
+            backgroundColor: '#28a745',
+            borderColor: '#28a745',
+        },
+        danger: {
+            color: '#fff',
+            backgroundColor: '#c82333',
+            borderColor: '#bd2130',
+        }
+    },
+
+    buttonsSizes: {
+        small:{
+            padding: '0.25rem 0.5rem',
+            fontSize: '.875rem',
+            lineHeight: '1.5',
+            borderRadius: '0.2rem',
+        },
+        large:{
+            padding: '0.5rem 1rem',
+            fontSize: '1.25rem',
+            lineHeight: '1.5',
+            borderRadius: '0.3rem',
+        },
+
+    },
+
+    buttonsActive: {
+        primary:{
+            color: '#fff',
+            backgroundColor: '#0062cc',
+            borderColor: '#005cbf',
+        },
+        success:{
+            color: '#fff',
+            backgroundColor: '#1e7e34',
+            borderColor: '#1c7430',
+        },
+        danger: {
+            color: '#fff',
+            backgroundColor: '#bd2130',
+            borderColor: '#b21f2d',
+        }
+    },
+
+}
