40c53a28d5622cdc1e9fd62c241ddfaba2df4bbd
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-09-25 13:31:35 +0200

    task 2 done

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Button from './../src/components/Button';
-import { Row, Col, Button as RBButton } from 'react-bootstrap';
+import Button from "./../src/components/Button";
+import { Row, Col, Button as RBButton } from "react-bootstrap";
 
 const Task02 = () => {
-    return (
-        <Row>
-            <Col>
-                <RBButton variant="primary" size="lg">Button!</RBButton>
-            </Col>
-            <Col>
-                Button!
-            </Col>
-        </Row>
-)
-}
+  return (
+    <Row>
+      <Col>
+        <RBButton variant="primary" size="lg">
+          Button!
+        </RBButton>
+      </Col>
+      <Col>
+        <Button variant="primary" size="small">
+          Button!
+        </Button>
+      </Col>
+    </Row>
+  );
+};
 
 export default Task02;
-
