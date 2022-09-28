0bb91e816694642c0d553ed902196636da9ef264
Magdalena Nolde <magdalena@nolde.de>
2022-09-28 14:29:24 +0200

    finish task 4

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,23 +1,41 @@
 import React from "react";
 import StyledTabs from "./Tabs.styled";
 import Tab from "./Tab.styled";
+import { useState } from "react";
 
-function Tabs() {
-  const [active, setActive] = useState("home");
-  const { children } = props;
+function Tabs({ children }) {
+  const [activeTab, setActiveTab] = useState(children[0].props.eventKey);
+
+  const handleClick = (e, newActiveTab) => {
+    e.preventDefault();
+    setActiveTab(newActiveTab);
+  };
+
   return (
-    <StyledTabs>
-      {children.map((eventKey, disabled, title) => (
-        <Tab
-          key={eventKey}
-          active={active === eventKey}
-          onClick={() => setActive(eventKey)}
-          disabled={disabled}
-        >
-          {title}
-        </Tab>
-      ))}
-    </StyledTabs>
+    <>
+      <StyledTabs>
+        {children.map((tab) => {
+          const { eventKey, title, active, disabled } = tab.props;
+          return (
+            <Tab
+              key={eventKey}
+              a
+              href="#"
+              onClick={(e) => handleClick(e, eventKey)}
+              active={eventKey === activeTab}
+              disabled={disabled}
+            >
+              {title}
+            </Tab>
+          );
+        })}
+      </StyledTabs>
+      {children.map((one) => {
+        if (one.props.eventKey === activeTab) {
+          return <p>{one.props.children}</p>;
+        }
+      })}
+    </>
   );
 }
 
