cec121dca0be839cc9e330ad0d31c31f20b573a1
Mateusz Suplewski <matx3582@gmail.com>
2022-09-28 12:02:25 +0200

    Task-1 Done

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { ThemeProvider } from 'styled-components';
 import { StyledAlert } from './Alert.styled';
 
+const themeSettings = {
+    primary: {
+    color: '#084298',
+    backgroundColor: '#cfe2ff',
+    borderColor: '#b6d4fe'
+    },
+    secondary: {
+    color: '#0f5132',
+    backgroundColor: '#d1e7dd',
+    borderColor: '#badbcc'
+    },
+}
+
+
 const Alert = props => {
+    const {variant} = props
     return (
-        <StyledAlert>{props.children}</StyledAlert>
+        <ThemeProvider theme={themeSettings}>
+            <StyledAlert variant={variant}>{props.children}</StyledAlert>
+        </ThemeProvider>
     );
 }
 
