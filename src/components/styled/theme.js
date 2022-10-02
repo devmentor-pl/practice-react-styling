a6cc7ca128763a9e60bebdbaac8ac3e669e5c372
Camila Montes Celinski <camicelinski@gmail.com>
2022-10-02 16:32:25 +0200

    Finish task 01

---
+++ src/components/styled/theme.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+const theme = {
+  alert: {
+    primary: css`
+      color: #084298;
+      background-color: #cfe2ff;
+      border-color: #b6d4fe;
+    `,
+    secondary: css`
+      color: #41464b;
+      background-color: #e2e3e5;
+      border-color: #d3d6d8;
+    `,
+  },
+};
+
+export default theme;
