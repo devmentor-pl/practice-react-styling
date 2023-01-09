7c5d434ab4aaaf5eee3058be26a0b8f6e42148b1
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-09 14:08:57 +0100

    Add task1

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { ThemeProvider } from 'styled-components';
 import { StyledAlert } from './Alert.styled';
 
-const Alert = props => {
+const theme = {
+    default: {
+        color: 'red',
+        backgroundColor: 'white',
+        borderColor: 'red',
+    },
+    primary: {
+        color: '#004085',
+        backgroundColor: '#cce5ff',
+        borderColor: '#b8daff',
+    },
+    secondary: {
+        color: '#383d41',
+        backgroundColor: '#e2e3e5',
+        borderColor: '#d6d8db'
+    }
+}
+
+
+const Alert = (props) => {
+    const {variant = "default"} = props
     return (
-        <StyledAlert>{props.children}</StyledAlert>
+        <ThemeProvider theme={theme[variant]}>
+            <StyledAlert>{props.children}</StyledAlert>
+        </ThemeProvider>
     );
 }
 
--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 
 const StyledAlert = styled.div`
     display: block;
+    position: relative;
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+    ${props => props.theme}
 `
 
 export { StyledAlert };
