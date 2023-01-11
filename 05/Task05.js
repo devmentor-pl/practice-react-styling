bc8de1a40ee513d6255f343352a5f5d2bb5fcbe1
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-11 16:01:57 +0100

    Import & Render Card component

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
-import { Row, Col, Card as RBCard, Button as RBButton } from 'react-bootstrap';
+import Card from '../src/components/Card/Card';
+import CardImage from '../src/components/Card/CardImg';
+import { Row, Col, Card as RBCard, Button as RBButton, CardImg } from 'react-bootstrap';
+import CardBody from '../src/components/Card/CardBody';
+import Button from '../src/components/Button/Button';
+import CardTitle from '../src/components/Card/CardTitle';
+import CardText from '../src/components/Card/CardText';
 
 const Task05 = () => {
     return (
@@ -19,7 +24,15 @@ const Task05 = () => {
                 </RBCard>
             </Col>
             <Col>
-                Card!
+                <Card style={{ width: '18rem' }}>
+                    <CardImage src="https://picsum.photos/100/80"/>
+                    <CardBody>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</CardText>
+                        <Button size="sm" variant="secondary">Go somewhere</Button>
+                    </CardBody>
+                </Card>
             </Col>
         </Row>
     )
