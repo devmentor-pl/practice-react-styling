a0f92aaf8276dcbd36f7e2b2e20ef3528aceb50b
Anna <aniamatrackasadurek@gmail.com>
2023-04-25 11:42:16 +0200

    not finish 05

---
+++ src/components/Card/CardTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledTitle = styled.h4`
+    margin-bottom: .85rem;
+`
+
+const CardTitle = () => {
+    return (
+        <StyledTitle>Card Title</StyledTitle>
+    )
+};
+
+export default CardTitle;
