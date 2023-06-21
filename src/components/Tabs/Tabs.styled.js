186af2aef7e27a82248e5746bfa94898269185f8
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-06-21 14:42:31 +0200

    Add ex 4

---
+++ src/components/Tabs/Tabs.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledTabs = styled.nav`
+	display: flex;
+	flex-wrap: wrap;
+	padding-left: 0;
+	margin-bottom: 0;
+	list-style: none;
+	border-bottom: 1px solid #dee2e6;
+`;
+
+export default StyledTabs;
