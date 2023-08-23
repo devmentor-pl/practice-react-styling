09096077bcc0a35fe5d8e9563221611578fc8cff
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-08-23 21:37:18 +0200

    task 1 added

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
-    display: block;
+const StyledAlertDefault = styled.div`
+    position: relative;
+    border: 1px solid black;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 1rem;
+    border-radius: 0.25rem;
 `
+
+const StyledAlert = styled(StyledAlertDefault)(
+    props => ({
+        backgroundColor: props.theme[props.variant + 'Background'],
+        border: props.theme[props.variant + 'Border'],
+        color: props.theme[props.variant + 'TextColor'],
+    })
+)
 
 export { StyledAlert };
