800d652d3981d99a5ac6ceffbef30e3ef71e0e4b
kozerka <kozerka@gmail.com>
2023-09-10 21:27:55 +0200

    Complete task 01

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,40 @@
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
-    display: block;
-`
+const DefaultStyledAlert = styled.div`
+	display: block;
+	position: relative;
+	padding: 0.75rem 1.25rem;
+	margin-bottom: 1rem;
+	border: 1px solid transparent;
+	border-radius: 0.25rem;
+`;
+
+// const StyledAlert = styled(DefaultStyledAlert)`
+// 	color: ${({ variant }) => {
+// 		if (variant === 'primary') return '#004085';
+// 		if (variant === 'secondary') return '#383d41';
+// 		return '#383838';
+// 	}};
+// 	background-color: ${({ variant }) => {
+// 		if (variant === 'primary') return '#cce5ff';
+// 		if (variant === 'secondary') return '#e2e3e5';
+// 		return '#e2e2e2';
+// 	}};
+// 	border-color: ${({ variant }) => {
+// 		if (variant === 'primary') return '#b8daff';
+// 		if (variant === 'secondary') return '#d6d8db';
+// 		return '#c2c2c2';
+// 	}};
+// `;
+const StyledAlert = styled(DefaultStyledAlert)`
+	color: ${({ theme, variant }) => theme.color[variant]?.color};
+	background-color: ${({ theme, variant }) =>
+		theme.color[variant]?.backgroundColor};
+	border-color: ${({ theme, variant }) => theme.color[variant]?.borderColor};
+`;
+
+
+
+
 
 export { StyledAlert };
---
+++ src/styles/themes.js
@@ -0,0 +1,34 @@
+export const themes = {
+	color: {
+		primary: {
+			color: '#004085',
+			backgroundColor: '#cce5ff',
+			borderColor: '#b8daff',
+		},
+		secondary: {
+			color: '#383d41',
+			backgroundColor: '#e2e3e5',
+			borderColor: '#d6d8db',
+		},
+		success: {
+			color: '#155724',
+			backgroundColor: '#d4edda',
+			borderColor: '#c3e6cb',
+		},
+		danger: {
+			color: '#721c24',
+			backgroundColor: '#f8d7da',
+			borderColor: '#f5c6cb',
+		},
+		warning: {
+			color: '#856404',
+			backgroundColor: '#fff3cd',
+			borderColor: '#ffeeba',
+		},
+		info: {
+			color: '#0c5460',
+			backgroundColor: '#d1ecf1',
+			borderColor: '#bee5eb',
+		},
+	},
+};
