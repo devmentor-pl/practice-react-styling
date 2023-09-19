d920e0edc00cea16b9d49f0309638bdb0a7bd1c2
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-09-19 18:37:30 +0200

    Finish task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,16 +1,31 @@
 import React from 'react';
-
+import { ThemeProvider } from 'styled-components';
 import Alert from './../src/components/Alert';
 import { Row, Col, Alert as RBAlert } from 'react-bootstrap';
 
 const Task01 = () => {
+    const themeSettings = {
+        primary: {
+            color: '#6EA8FE',
+            backgroundColor: '#031633',
+            border: '1px solid #084298',
+        },
+        secondary: {
+            color: '#A7ACB1',
+            backgroundColor: '#161719',
+            border: '1px solid #41464B',
+        }
+
+    }
     return (
         <Row>
             <Col>
                 <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
             </Col>
             <Col>
-                <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                <ThemeProvider theme={themeSettings}>
+                    <Alert >Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
+                </ThemeProvider>
             </Col>
         </Row>
     )
--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledAlert = styled.div`
     display: block;
+    margin-bottom: 16px;
+    padding: 12px 20px;
+    color: #004085;
+    background-color:  #CCE5FF;
+    border-radius: 4px;
+    border: 1px solid #B8DAFF;
+    ${props => {
+        if (props.variant === 'primary') {
+            return props.theme.primary
+        } else if (props.variant === 'secondary') {
+            return props.theme.secondary
+        }
+    }}
 `
 
 export { StyledAlert };
