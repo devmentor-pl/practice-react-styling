a4633e895a80f2f7cf709e920e1eb3e9a8f467ea
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-09-22 10:08:21 +0200

    Improve code

--- src/components/Tabs/Tab.js
+++ src/components/Tabs/Tab.js
@@ -2,7 +2,7 @@ import React from "react";
 import StyledTab from "./Tab.styled";
 
 const Tab = ({ onClick, title, name, active, children, disabled }) => {
-    return <StyledTab name={name} active={active} onClick={disabled ? () => { } : onClick} title={title} disabled={disabled}>
+    return <StyledTab name={name} active={active} onClick={disabled ? null : onClick} title={title} disabled={disabled}>
         {children}
     </StyledTab>
 }
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,29 +2,8 @@ import React, { useState } from "react";
 import StyledTabs from "./Tabs.styled";
 import Tab from "./Tab";
 
-const tabsList = [
-    {
-        id: 1,
-        name: 'home',
-        title: 'Home',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum lacus nec ligula faucibus rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ',
-        disabled: false,
-    },
-    {
-        id: 2,
-        name: 'profile',
-        title: 'Profile',
-        text: 'Donec dignissim ultricies felis, eu dictum eros congue in. In gravida lobortis libero nec tempus. Cras rutrum nisl ut leo volutpat rhoncus. Nulla massa nulla, viverra hendrerit laoreet at, tincidunt eu lacus.',
-        disabled: false,
-    },
-    {
-        id: 3,
-        name: 'contact',
-        title: 'Contact',
-        text: 'Vivamus metus nulla, fermentum eget placerat vitae, mollis interdum elit. Pellentesque arcu augue, vulputate ut porttitor ut, suscipit non orci. Integer justo odio, suscipit eget tortor nec, molestie lobortis eros. Nullam commodo elit sit amet lacus blandit aliquet. Mauris at nibh eget nisl pulvinar dignissim.',
-        disabled: true,
-    }
-]
+import tabsList from "./tablist";
+
 
 const Tabs = () => {
     const [activeTab, setActiveTab] = useState('profile')
