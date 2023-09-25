23e1af98af04b00c1869434722ebfdb392d54d0f
kozerka <kozerka@gmail.com>
2023-09-25 22:36:23 +0200

    Add focus to button

--- 02/Task02.js
+++ 02/Task02.js
@@ -5,19 +5,19 @@ import { Row, Col, Button as RBButton } from 'react-bootstrap';
 
 const Task02 = () => {
     return (
-			<Row>
-				<Col>
-					<RBButton variant="success" size="md">
-						Button!
-					</RBButton>
-				</Col>
-				<Col>
-					<Button variant="success" size="md">
-						Button!
-					</Button>
-				</Col>
-			</Row>
-		);
+      <Row>
+        <Col>
+          <RBButton variant="success" size="md">
+            Button!
+          </RBButton>
+        </Col>
+        <Col>
+          <Button variant="success" size="md">
+            Button!
+          </Button>
+        </Col>
+      </Row>
+    );
 }
 
 export default Task02;
--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,90 +1,107 @@
 export const themes = {
-	alert: {
-		primary: {
-			color: '#004085',
-			backgroundColor: '#cce5ff',
-			borderColor: '#b8daff',
-		},
-		secondary: {
-			color: '#383d41',
-			backgroundColor: '#e2e3e5',
-			borderColor: '#d6d8db',
-		},
-		success: {
-			color: '#155724',
-			backgroundColor: '#d4edda',
-			borderColor: '#c3e6cb',
-		},
-		danger: {
-			color: '#721c24',
-			backgroundColor: '#f8d7da',
-			borderColor: '#f5c6cb',
-		},
-		warning: {
-			color: '#856404',
-			backgroundColor: '#fff3cd',
-			borderColor: '#ffeeba',
-		},
-		info: {
-			color: '#0c5460',
-			backgroundColor: '#d1ecf1',
-			borderColor: '#bee5eb',
-		},
-	},
-	button: {
-		sizes: {
-			sm: {
-				fontSize: '.875rem',
-				padding: '0.25rem 0.5rem',
-			},
-			md: {
-				fontSize: '1rem',
-				padding: '0.375rem 0.75rem',
-			},
-			lg: {
-				fontSize: '1.25rem',
-				lineHeight: '1.5',
-				padding: '0.5rem 1rem',
-			},
-		},
-		primary: {
-			color: '#fff',
-			backgroundColor: '#007bff',
-			borderColor: 'transparent',
-			hover: {
-				backgroundColor: '#0069d9',
-				borderColor: 'transparent',
-			},
-			active: {
-				backgroundColor: '#0069d9',
-				borderColor: 'transparent',
-			},
-		},
-		secondary: {
-			color: '#fff',
-			backgroundColor: '#6c757d',
-			borderColor: 'transparent',
-			hover: {
-				backgroundColor: '#5a6268',
-				borderColor: 'transparent',
-			},
-			active: {
-				backgroundColor: '#545b62',
-				borderColor: 'transparent',
-			},
-		},
-		success: {
-			color: '#fff',
-			backgroundColor: '#28a745',
-			borderColor: 'transparent',
-			hover: {
-				backgroundColor: '#218838',
-				borderColor: 'transparent',
-			},
-			active: {
-				backgroundColor: '#1e7e34',
-				borderColor: 'transparent',
-			},
-		},
-	},
+  alert: {
+    primary: {
+      color: "#004085",
+      backgroundColor: "#cce5ff",
+      borderColor: "#b8daff",
+    },
+    secondary: {
+      color: "#383d41",
+      backgroundColor: "#e2e3e5",
+      borderColor: "#d6d8db",
+    },
+    success: {
+      color: "#155724",
+      backgroundColor: "#d4edda",
+      borderColor: "#c3e6cb",
+    },
+    danger: {
+      color: "#721c24",
+      backgroundColor: "#f8d7da",
+      borderColor: "#f5c6cb",
+    },
+    warning: {
+      color: "#856404",
+      backgroundColor: "#fff3cd",
+      borderColor: "#ffeeba",
+    },
+    info: {
+      color: "#0c5460",
+      backgroundColor: "#d1ecf1",
+      borderColor: "#bee5eb",
+    },
+  },
+  button: {
+    sizes: {
+      sm: {
+        fontSize: ".875rem",
+        padding: "0.25rem 0.5rem",
+      },
+      md: {
+        fontSize: "1rem",
+        padding: "0.375rem 0.75rem",
+      },
+      lg: {
+        fontSize: "1.25rem",
+        lineHeight: "1.5",
+        padding: "0.5rem 1rem",
+      },
+    },
+    primary: {
+      color: "#fff",
+      backgroundColor: "#007bff",
+      borderColor: "transparent",
+      hover: {
+        backgroundColor: "#0069d9",
+        borderColor: "transparent",
+      },
+      active: {
+        backgroundColor: "#0069d9",
+        borderColor: "transparent",
+      },
+      focus: {
+        backgroundColor: "#0069d9",
+        borderColor: "transparent",
+        boxShadow: "0 0 0 0.2rem rgba(38, 143, 255, 0.5)",
+      },
+    },
+    secondary: {
+      color: "#fff",
+      backgroundColor: "#6c757d",
+      borderColor: "transparent",
+      hover: {
+        backgroundColor: "#5a6268",
+        borderColor: "transparent",
+      },
+      active: {
+        backgroundColor: "#545b62",
+        borderColor: "transparent",
+      },
+      focus: {
+        backgroundColor: "#545b62",
+        borderColor: "transparent",
+        boxShadow: "0 0 0 0.2rem rgba(108, 117, 125, 0.5)",
+      },
+    },
+    success: {
+      color: "#fff",
+      backgroundColor: "#28a745",
+      borderColor: "transparent",
+      hover: {
+        backgroundColor: "#218838",
+        borderColor: "transparent",
+      },
+      active: {
+        backgroundColor: "#1e7e34",
+        borderColor: "transparent",
+        outline: "2px solid #20c997",
+      },
+      focus: {
+        backgroundColor: "#1e7e34",
+        borderColor: "transparent",
+        outline: 0,
+        boxShadow: "0 0 0 0.2rem rgba(40, 167, 69, 0.5)",
+      },
+    },
+  },
 };
