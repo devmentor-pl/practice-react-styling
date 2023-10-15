b02ccff37454d04ca3787056d7a706d5e8637af1
Marcin <xcinek1337@wp.pl>
2023-10-15 12:41:56 +0200

    02 with provider

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-import { variant } from './../src/components/Button/Button.styled';
+import { theme } from './../src/components/Button/Button.styled';
 
 import Button from './../src/components/Button';
 import { Row, Col, Button as RBButton } from 'react-bootstrap';
@@ -22,19 +22,14 @@ const Task02 = () => {
 				<RBButton variant='primary'>Button!</RBButton>
 			</Col>
 			<Col>
-				{/* w buttonach nie mozna stylami przesylac, bo nadpisza inline stylami i w tedy active i inne pseudoklasy musialby byc stytlowaNE za pomoca important, wiec zostaje takie rozwiazanie, button musi zostac owrapowany themeProviderem, i w themeProvider sie podaje wariant zamiast w atrybucie przycisku... */}
-				{/* ale za to Button ma swoje customowe style jesli jest poza themeProviderem... */}
-				<ThemeProvider theme={variant.secondary}>
-					<Button size='lg'>Button!</Button>
-					<Button active={true} size='sm'>
+				<ThemeProvider theme={theme}>
+					<Button size='lg' variant='secondary'>
 						Button!
 					</Button>
-					<Button disabled>Button!</Button>
+					<Button size='sm' active variant={'secondary'}>Button!</Button>
+					<Button disabled variant={'secondary'}>Button!</Button>
+					<Button variant={'primary'}>Button!</Button>
 				</ThemeProvider>
-				<ThemeProvider theme={variant.primary}>
-					<Button>Button!</Button>
-				</ThemeProvider>
-				{/* <Button>DEFAULT</Button> */}
 			</Col>
 		</Row>
 	);
