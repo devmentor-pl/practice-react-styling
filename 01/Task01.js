c6b356d9d2f1f4e22d1a43383abc4ac4537f6341
Maciej Necka <macieknecka@gmail.com>
2023-12-11 10:23:09 +0100

    Task 01 - Done

--- 01/Task01.js
+++ 01/Task01.js
@@ -4,17 +4,25 @@ import Alert from './../src/components/Alert';
 import { Row, Col, Alert as RBAlert } from 'react-bootstrap';
 
 const Task01 = () => {
-    return (
-        <Row>
-            <Col>
-                <RBAlert variant="primary">Uwaga! <em>Styled Components</em> nadchodzi!</RBAlert>
-            </Col>
-            <Col>
-                <Alert>Uwaga! <em>Styled Components</em> nadchodzi!</Alert>
-            </Col>
-        </Row>
-    )
-}
+  const theme = {
+    primary: '#007bff',
+    secondary: '#6c757d',
+    default: '#343a40',
+  };
+  return (
+    <Row>
+      <Col>
+        <RBAlert variant="primary">
+          Uwaga! <em>Styled Components</em> nadchodzi!
+        </RBAlert>
+      </Col>
+      <Col>
+        <Alert variant="secondary" theme={theme}>
+          Uwaga! <em>Styled Components</em> nadchodzi!
+        </Alert>
+      </Col>
+    </Row>
+  );
+};
 
 export default Task01;
-
--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { ThemeProvider } from 'styled-components';
 import { StyledAlert } from './Alert.styled';
 
-const Alert = props => {
-    return (
-        <StyledAlert>{props.children}</StyledAlert>
-    );
-}
+const Alert = (props) => {
+  return (
+    <ThemeProvider theme={props.theme || {}}>
+      <StyledAlert variant={props.variant}>{props.children}</StyledAlert>
+    </ThemeProvider>
+  );
+};
 
 export default Alert;
