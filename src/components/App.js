229cc112d3d64f71938523a63aa30e62e1d72c8e
Kaczupinko <kaczupinko@gmail.com>
2024-01-10 18:20:03 +0000

    Finish task 01

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import { ThemeProvider } from 'styled-components';
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
+const theme = {
+    primary: '#007bff',
+    primaryDark: '#0056b3',
+    primaryHover: '#0069d9',
+    secondary: '#6c757d',
+    secondaryDark: '#545b62',
+    secondaryHover: '#5a6268',
+};
 
 const App = () => {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <Container fluid>
                 <Row>
                     <Col>
@@ -25,7 +33,7 @@ const App = () => {
                 <Task04/>
                 <Task05/>
             </Container>
-        </>
+        </ThemeProvider>
     )
 }
 
