410ed8798fd585c830874087c437b50b0b2bb6a9
Michal Gawlik <gawlik.zator@o2.pl>
2024-01-25 09:05:38 +0100

    task-01-done

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import { StyledAlert } from './Alert.styled';
+import { StyledAlert } from './Alert.styled'
+import { ThemeProvider } from 'styled-components'
+
+const themeSettings = {
+	colorAlfa: 'royalblue',
+}
 
 const Alert = props => {
-    return (
-        <StyledAlert>{props.children}</StyledAlert>
-    );
+	return (
+		<ThemeProvider theme={themeSettings}>
+			<StyledAlert variant="primary">{props.children}</StyledAlert>
+		</ThemeProvider>
+	)
 }
 
 export default Alert;
